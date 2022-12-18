3e0618cb86abcae4784a3ad53ea473daac767490
ghostmodd <ghost1mod@gmail.com>
2022-12-18 07:50:56 +0300

    fix: fixed index.js, index.html  and rewrote styles

--- scripts/index.js
+++ scripts/index.js
@@ -1,57 +1,41 @@
 // значения профиля
 const userName = document.querySelector('.profile__user-name');
-const userNameInput = document.querySelector('.popup__input_type_user-name');
+const userNameInput = document.querySelector('input[name="user-name"]');
 const userAbout = document.querySelector('.profile__about-user');
-const userAboutInput = document.querySelector('.popup__input_type_about-user');
+const userAboutInput = document.querySelector('input[name="about-user"]');
 
 // управление popup`ом
 const popup = document.querySelector('.popup');
+const formEditProfile = document.querySelector('.edit-profile');
 const buttonShowPopup = document.querySelector('.profile__btn-edit-profile');
-const buttonClosePopup = document.querySelector('.popup__btn-close');
+const buttonClosePopup = document.querySelector('.edit-profile__btn-close');
 
 
 // добавить значения к инпутам
 function addValuePopup() {
-  userNameInput.setAttribute('value', `${userName.textContent}`);
-  userAboutInput.setAttribute('value', `${userAbout.textContent}`);
+  userNameInput.value = `${userName.textContent}`;
+  userAboutInput.value = `${userAbout.textContent}`;
 };
 
 // показать или скрыть форму
 function callPopup() {
-  debugger
-  popup.classList.toggle('popup_opened');
+  if(!popup.classList.contains('popup_opened')) {
+    addValuePopup();
+    popup.classList.add('popup_opened');
+  } else {
+    popup.classList.remove('popup_opened');
+  }
 };
 
 // отправка формы
 function submitPopup(evt) {
   evt.preventDefault();
-  editProfile();
+  userName.textContent = userNameInput.value;
+  userAbout.textContent = userAboutInput.value;
   callPopup();
 };
 
-
-// изменение данных профиля
-function editProfile() {
-  userName.textContent = userNameInput.value;
-  userAbout.textContent = userAboutInput.value;
-}
-
-// реализация горячих клавиш
-// думаю, в будущем смогу оптимизировать с помощью асинхронного программирования. поставить бы
-// цикл какой-нибудь, чтоб Listener ловил события только если popup открыт
-function ifHotKeyPressedPopup(evt) {
-  if (popup.classList.contains('popup_opened') && evt.key === 'Enter') {
-    editProfile();
-  }
-
-  else if (popup.classList.contains('popup_opened') && evt.key === 'Escape') {
-    callPopup();
-  }
-};
-
 // Listener`ы и иное взаимодействие с DOM
-addValuePopup();
 buttonShowPopup.addEventListener('click', callPopup);
-popup.addEventListener('submit', submitPopup);
+formEditProfile.addEventListener('submit', submitPopup);
 buttonClosePopup.addEventListener('click', callPopup);
-document.addEventListener('keydown', ifHotKeyPressedPopup);
